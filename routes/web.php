bd96b5128d24d47a982a1dd51dddb4e08f52b650
Muhammad Nazar Abdillah <muhammad.nazar.abd20@gmail.com>
2024-09-05 13:26:01 +0800

    feat: book feature

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 
@@ -14,12 +15,22 @@ Route::get('/dashboard', function () {
 
 
 Route::middleware('auth')->group(function () {
+    // route user
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
     Route::post('/user', [UserController::class, 'store'])->name('user.store');
     Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
-    Route::put('/user/{user}', [UserController::class, 'update'])->name('user.update');
+    Route::put('/user/{user}/edit', [UserController::class, 'update'])->name('user.update');
     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+
+    // book route
+    Route::get('/book', [BookController::class, 'index'])->name('book.index');
+    Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
+    Route::post('/book', [BookController::class, 'store'])->name('book.store');
+    Route::get('/book/{book}/edit', [BookController::class, 'edit'])->name('book.edit');
+    Route::put('/book/{book}/edit', [BookController::class, 'update'])->name('book.update');
+    Route::delete('/book/{book}', [BookController::class, 'destroy'])->name('book.destroy');
+
 
     
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
